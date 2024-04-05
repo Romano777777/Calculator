9a1b13aafa68d222c2bc226d57d595e8928af58c
Romano <br0567575@gmail.com>
2024-04-05 09:18:25 -0300

    calculator

--- src/components/CalculatorPage.vue
+++ src/components/CalculatorPage.vue
@@ -8,26 +8,32 @@
             v-model="value2"
             label="valor2"
         ></v-text-field>
-        <v-btn @click="Calc('soma')">
-            Soma
-            
-        </v-btn>
-        <v-btn @click="Calc('subtração')">
-            Subtração
 
-        </v-btn>
-        <v-btn @click="Calc('multiplicação')">
-            Multiplicação
-
-        </v-btn>
-        <v-btn @click="Calc('divisão')" >
-            Divisão
-
-        </v-btn>
-        <div>
+        <div class="container-result">
             {{result}}
         </div>
-    </div>
+
+        <div class="operation-container">
+            <div class="operation-item" :class="{'operation-active':operation === 'soma'}" @click="Calc('soma')" >
+                <v-icon icon="mdi-plus" size="80" ></v-icon>
+                <span>Somar</span>
+            </div>
+            <div class="operation-item" :class="{'operation-active':operation === 'subtração'}"  @click="Calc('subtração')">
+                <v-icon icon="mdi-minus" size="80" ></v-icon>
+                <span>Subtração</span>
+            </div>
+            <div class="operation-item" :class="{'operation-active':operation === 'multiplicação'}" @click="Calc('multiplicação')">
+                <v-icon icon="mdi-multiplication" size="80" ></v-icon>
+                <span>Multiplicação</span>
+            </div>
+            <div class="operation-item" :class="{'operation-active':operation === 'divisão'}" @click="Calc('divisão')">
+                <v-icon icon="mdi-division" size="80" ></v-icon>
+                <span>Divisão</span>
+            </div>
+        </div>
+
+            
+        </div>
 </template>
 
 <script>
@@ -36,6 +42,7 @@
     export default defineComponent({ 
         data() {
             return {
+                operation: null,
                 result: null, 
                value1: null, 
                value2: null
@@ -46,11 +53,12 @@
         },
         methods: {
             Calc(operacao) {
+                this.operation = operacao
 
-                if (operacao === 'soma') return this.result = Number(this.value1) + Number(this.value2)
-                if (operacao === 'subtração') return this.result = Number(this.value1) - Number(this.value2)
-                if (operacao === 'multiplicação') return this.result = Number(this.value1) * Number(this.value2)
-                if (operacao === 'divisão') return this.result = Number(this.value1) / Number(this.value2)
+                if (operacao === 'soma') return this.result = `${this.value1} + ${this.value2} = ${Number(this.value1) + Number(this.value2)} `
+                if (operacao === 'subtração') return this.result = `${this.value1} - ${this.value2} = ${Number(this.value1) - Number(this.value2)} `
+                if (operacao === 'multiplicação') return this.result = `${this.value1} x ${this.value2} = ${Number(this.value1) * Number(this.value2)} `
+                if (operacao === 'divisão') return this.result = `${this.value1} / ${this.value2} = ${Number(this.value1) / Number(this.value2)} `
                 
             }
             
@@ -60,6 +68,48 @@
 
 </script>
 
-<style>
+<style lang="scss" >
+@import url('https://fonts.googleapis.com/css2?family=Aclonica&family=Advent+Pro:ital,wght@0,100..900;1,100..900&family=Tilt+Neon&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Aclonica&family=Advent+Pro:ital,wght@0,100..900;1,100..900&family=Tac+One&family=Tilt+Neon&display=swap');
+
+.operation-container {
+    padding: 0px 50px;
+    display: flex;
+    justify-content: space-between;
+
+    .operation-item {
+        cursor: pointer;
+        font-family: "Tilt Neon", sans-serif;
+        font-size: 26px;
+        flex-direction: column;
+        justify-content: center;
+        display: flex;
+        align-items: center;
+        border-radius: 10px;
+        width: 250px;
+        height: 200px;
+        background-color: #659FA6;
+        &:hover {
+            background-color: #6aa2a8;
+        }
+    }
+    .operation-active {
+        background-color: #308C83;
+    }
+}
+
+.container-result { 
+    margin-bottom: 50px;
+    color: #155952;
+    font-size: 120px;
+    font-family: "Tac One", sans-serif;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 400px;
+    background-color: #B4D9CE;
+}
+
 
 </style>
